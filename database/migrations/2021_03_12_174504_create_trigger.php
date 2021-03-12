1e6c5ee16251225e4d5bbee62b58d986d6167b39
AndreyVitovtov <andrey.vitovtov@gmail.com>
2021-03-12 19:57:00 +0200

    add migration create trigger ref system

---
+++ database/migrations/2021_03_12_174504_create_trigger.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateTrigger extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared("
+            CREATE TRIGGER `referral_system_AFTER_INSERT` AFTER INSERT ON `referral_system` FOR EACH ROW
+            BEGIN
+                UPDATE `users` SET `count_ref` = (
+                    SELECT COUNT(*)
+                    FROM `referral_system`
+                    WHERE `referrer` = NEW.referrer
+                )
+                WHERE `id` = NEW.referrer
+            END
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER `referral_system_AFTER_INSERT`');
+    }
+}
